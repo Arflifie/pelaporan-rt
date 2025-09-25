27858f63b601151e9ac070480bbe32dfd2270103
Arfun Ali Yafie <alinews120@gmail.com>
2025-09-25 17:34:22 +0700

    nyesuain auth belum selesai

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,17 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('dashboard');
 });
+
 Route::get('/form', function () {
     return view('form');
+});
+
+Route::get('/auth.login', function () {
+    return view('login');
+});
+
+Route::get('/auth.register', function () {
+    return view('register');
 });
 
 ?>
