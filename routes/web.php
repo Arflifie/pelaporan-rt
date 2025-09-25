cc13442990c40a8d64c19f32effb4d95f2824900
Arfun Ali Yafie <alinews120@gmail.com>
2025-09-25 17:17:56 +0700

    buat blade template untuk navbar

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-Route::get('/home', function(){
-    return view('halaman_utama');
+Route::get('/form', function () {
+    return view('form');
 });
+
+?>
