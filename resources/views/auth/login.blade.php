a790bf0eab66fcf3734e3d561a2def78cf9bc5ad
Arfun Ali Yafie <alinews120@gmail.com>
2025-10-24 19:35:35 +0700

    halaman login belum responsive

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,21 +1,38 @@
 @extends('layouts.layoutauth')
 @section('content')
-    <form action="" class="py-06">
+<div class="absolute z-0 p-60 sm:p-30 bg-blue-950 -ml-35 sm:-ml-50 -mt-20 rounded-full">
+        {{-- dekorasi1 --}}
+    </div>
+<div class="z-20 flex justify-center">
+    <form action="" class=" z-20 mt-30">
+        <h1 class="text-white text-center">SiagaRT</h1>
         <h2 class="text-white text-center">Form Login</h2>
-        <div class="text-center">
-            <label for="name" class="">Username</label><br>
-            <input class="py-1 px-10"
-            type="text" id="name" placeholder="masukkan username" required>
-        </div>
-        <div class="text-center">
-            <label for="password">Password</label> <br>
-            <input class="py-1 px-10"
-            type="password" id="password" placeholder="masukkan password" required>
+        <div class="">
+            <label for="name" class="ml-2 text-xl text-white">Username</label><br>
+            <div class="">
+                <input class="py-2.5 w-110 sm:w-5 px-3 bg-white rounded-lg"
+                type="text" id="name" placeholder="masukkan username" required>
+            </div>
+        </div><br>
+        <div class="">
+            <label for="password" class="ml-2 text-xl text-white">Password</label> <br>
+            <div class="">
+                <input class=" py-2.5 w-110 px-3 bg-white rounded-lg focus:ring-2 focus:outline-none"
+                type="password" id="password" placeholder="masukkan password" required>
+            </div>
+            
         </div> <br>
-        <div type="submit" class="text-center rounded-lg px-2 bg-blue-400 py-1 text-white hover:bg-blue-500 transition-colors">
-            Login
+        <div class="flex justify-center">
+            <div 
+                type="submit" class=" w-50 mt-25 text-center rounded-2xl bg-green-400 py-3 text-white hover:bg-green-700 transition-colors">
+                Login
+            </div>
         </div>
-        </div>
-        
     </form>
+</div>
+    <div class="absolute z-10 p-60 bg-orange-100 -mt-50 ml-60 rounded-full">    
+        {{-- dekorasi2 --}}
+    </div>
+
+    
 @endsection
--- resources/views/layouts/layoutauth.blade.php
+++ resources/views/layouts/layoutauth.blade.php
@@ -5,10 +5,16 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>testing @yield(section: 'tittle')</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    {{-- font montserrat --}}
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&family=Oswald:wght@200..700&family=Quicksand:wght@300..700&family=Roboto:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+
+    {{-- tailwindcss --}}
     @vite('resources/css/app.css', 'resources/js/app.js')
   </head>
   <body>
-    <div class: container class="bg-yellow-400 min-h-screen">
+    <div class: container class="relative bg-yellow-500 min-h-screen overflow-hidden">
       @yield(section: 'content')
     </div>
     <div class="footer">
